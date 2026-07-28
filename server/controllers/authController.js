e4faac6e55ddfaf9f07326f7b7ba0efcd506be11
kiranvajja16 <vajjakiran2005@gmail.com>
2026-07-28 15:21:26 +0530

    controller and routes are created

---
+++ server/controllers/authController.js
@@ -0,0 +1,41 @@
+const User=require("../models/User");
+const bcrypt=require("bcryptjs");
+
+const registerUser=async(req,res)=>{
+    try{
+        const {name,email,password,role}=req.body;
+        if(!name || !email || !password){
+            return res.status(400).json({
+                message:"Please fill all required fields",
+            });
+        }
+        const existingUser=await User.findOne({email});
+        if(existingUser){
+            return res.status(400).json({
+                message:"User already exists",
+            });
+        }
+        const hashedPassword = await bcrypt.hash(password,10);
+
+        const user=await User.create({
+            name,
+            email,
+            password:hashedPassword,
+            role,
+        });
+        res.status(201).json({
+            success:true,
+            message:"User registered successfully",
+            user,
+        });
+    }
+    catch(error){
+        res.status(500).json({
+            message:error.message,
+        });
+    }
+};
+
+module.exports={
+    registerUser,
+};
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const cors=require("cors");
+const authRoutes=require("./routes/authRoutes");
 require("dotenv").config();
 console.log(process.env.MONGO_URI);
 
@@ -10,6 +11,7 @@ connectDB()
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth",authRoutes);
 
 app.get("/",(req,res)=>{
     res.send("Support Ticket System API is running...");
